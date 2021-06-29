51141ae1a52826d75174a7f0e720e938cdcaeb17
Maya <62585570+noob-entity@users.noreply.github.com>
2021-06-29 16:28:32 +0530

    Next Greater Element To The Right

---
+++ Stack and Queue/NGE.java
@@ -0,0 +1,22 @@
+ public static int[] solve(int[] arr){
+   // solve
+   int[] res=new int[arr.length];
+   Stack<Integer> st=new Stack<>();
+   res[arr.length-1]=-1;
+   for(int i=arr.length-1;i>=0;i--){
+       //pop all the smaller elements
+       while(st.size()>0&&st.peek()<arr[i]){
+           st.pop();
+       }
+       //update result 
+       //if empty -1
+       //else top of stack
+       if(st.isEmpty())
+       res[i]=-1;
+       else
+       res[i]=st.peek();
+       //push current element to the stack
+       st.push(arr[i]);
+   }
+   return res;
+ }
