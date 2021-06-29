8c8305159c14ad4f61005dfa7b4efb3f411821a2
Maya <62585570+noob-entity@users.noreply.github.com>
2021-06-29 16:09:16 +0530

    Balanced Brackets

---
+++ Stack and Queue/Balanced.java
@@ -0,0 +1,28 @@
+    public static boolean isDuplicate(String str){
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='('||str.charAt(i)=='['||str.charAt(i)=='{'){
+            st.push(str.charAt(i));
+            }
+            else if(str.charAt(i)==')'||str.charAt(i)==']'||str.charAt(i)=='}'){
+                if(st.isEmpty())
+                return false;
+            char c=st.peek();
+            if(!isMatch(c,str.charAt(i)))
+            return false;
+            st.pop();
+            }
+   }
+    
+        return st.isEmpty();
+
+    }
+    public static boolean isMatch(char c,char ch){
+        if(c=='{'&&ch=='}')
+        return true;
+        else if(c=='['&&ch==']')
+        return true;
+        else if(c=='('&&ch==')')
+        return true;
+        else return false;
+    }
